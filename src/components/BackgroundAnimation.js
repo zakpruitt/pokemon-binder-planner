0dd9d9164a3a31f578edc3024f978c31101c464b
Zak Pruitt <pruittzn@gmail.com>
2025-02-25 04:45:55 -0600

    clean up BackgroundAnimation.js

--- src/components/BackgroundAnimation.js
+++ src/components/BackgroundAnimation.js
@@ -1,21 +1,19 @@
 // src/BackgroundAnimation.js
-import React, { useEffect, useState } from 'react';
-import '../styles/BackgroundAnimation.css';
-import {SPRITE_URLS} from "../utils/SpriteUrls";
+import React, { useState } from "react";
+import "../styles/BackgroundAnimation.css";
+import { SPRITE_URLS } from "../utils/SpriteUrls";
 
 function BackgroundAnimation() {
-    const [spriteUrl, setSpriteUrl] = useState('');
+    const getRandomSprite = () =>
+        SPRITE_URLS[Math.floor(Math.random() * SPRITE_URLS.length)];
 
-    useEffect(() => {
-        const randomUrl = SPRITE_URLS[Math.floor(Math.random() * SPRITE_URLS.length)];
-        setSpriteUrl(randomUrl);
-    }, []);
+    const [spriteUrl] = useState(getRandomSprite);
 
     return (
         <div
             className="background-animation"
-            style={{ backgroundImage: spriteUrl ? `url(${spriteUrl})` : 'none' }}
-        ></div>
+            style={{ backgroundImage: spriteUrl ? `url(${spriteUrl})` : "none" }}
+        />
     );
 }
 
